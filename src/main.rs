8827b2267b7e6828776fd0e8874c64cc507ba069
Eli Lipsitz <eli.lipsitz@gmail.com>
2020-03-01 09:08:06 -0800

    Read filename from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{env, process};
+use std::fs::File;
+
+struct Config {
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        }
+
+        Ok(Config {
+            filename: args[1].clone(),
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Reading {}", config.filename);
 }
